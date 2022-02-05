5d52dec38b297906a44c77ad970b37534154a979
BassamMutairi <mutairibassam@gmail.com>
2022-02-06 00:09:30 +0300

    Update: added constant file

---
+++ app/constants.js
@@ -0,0 +1,13 @@
+
+const Databases = {
+    FAKEY: "fakey"
+}
+
+const Collections = {
+    ACCOUNTS: "accounts",
+    EVENTS: "events",
+    TICKETS: "tickets",
+    USERS: "users"
+}
+
+module.exports = { Databases, Collections}
--- app/server.js
+++ app/server.js
@@ -1,5 +1,7 @@
 // declare function require(name:string);
+const { log } = require('console');
 const { MongoClient } = require('mongodb');
+const { Databases, Collections } = require('./constants');
 require("dotenv").config()
 
 /**
@@ -29,8 +31,10 @@ async function main(){
         // fetch user details based on username
         await showUserByUsername(client, "mmingeth");
 
-        const _res = await userLoginValidation(client, "mmingeth", "GDdPxZ")
-        console.log(_res)
+        const _res = await userLoginValidation(client, "mmingeth", "gDdPxZ")
+        // proper msg
+        if(_res == true) console.log("Correct credential");
+        else console.log("Incorrect credential");
 
     } catch (e) {
         console.error(e);
@@ -55,15 +59,17 @@ async function showUserByUsername(client, _username) {
      *  [ "accounts", "events", "tickets", "users" ]
      */
 
-    const res = await client.db("fakey")
-    .collection("accounts")
+    const res = await client.db(Databases.FAKEY)
+    .collection(Collections.ACCOUNTS)
     .findOne({ username: _username });
-    console.log(res)
+    console.log("user details:")
+    console.log(res);
 }
 
 async function userLoginValidation(client, _username, _password) {
-    const res = await client.db("fakey")
-                            .collection("accounts")
+
+    const res = await client.db(Databases.FAKEY)
+                            .collection(Collections.ACCOUNTS)
                             .findOne({ username: _username })
     const pass = res.password
     if(pass == _password) return true 
